19d0d093b92e80a9beb991826dc7ccfd03d6a148
mwangangi <muindemwangangi2@gmail.com>
2025-08-21 10:17:21 +0300

    adddingf schema

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,10 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import { CreateVadorInput } from "../dto";
+import { Vandor } from "../models";
 
 
 export const CreateVador = async (req: Request, res: Response, next: NextFunction) => {
 
     const { name, email, password, address, foodtype, phone, ownerName, pincode } = <CreateVadorInput>req.body;
+
+
+    const CreateVador = await Vandor.create({
+        name: name,
+        email: email,
+        password: password,
+        address: address,
+        phone: phone,
+        ownerName: ownerName,
+        pincode: pincode,
+        coverImage: [],
+        foodtype: foodtype ? foodtype.split(',') : [],
+        serviceAvailable: false,
+        salt: "random_salt_value", // Replace with actual salt generation logic
+        rating: 0
+    })
     return res.json({
         name,
         email,
